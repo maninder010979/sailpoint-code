934f1a82459841a456e289d5b28622b10d5f5914
Maninder  Pal Singh <maninderpalsingh@Maninders-Mini.phub.net.cable.rogers.com>
2021-10-22 18:52:30 -0400

    included the console print

--- python/constant.py
+++ python/constant.py
@@ -1,4 +1,10 @@
 # this file will have all constant variable information 
 git_pull_requst_repo="https://api.github.com/repos/maninder010979/samples/pulls?state="
 pull_request_type ="all" # can be open,closed or all
+str_display_mode ="console" # can be console or email
 
+############################
+### constant for the printing the data in console 
+str_console_print = "Hi All , \n\n Please find below the summary details  of the pull request created with status and information\n\n"
+str_console_print += "PR Number\tTitle\t\tRaisedby\t\tPull-Request-Url\t\t\t\t\tCodeurl\t\t\t"
+str_console_footer= "\n Thanks and Regards\nManidner Pal Singh"
--- python/function.py
+++ python/function.py
@@ -16,10 +16,14 @@ def getapi():
 
 def parsejson(pulljson):
     try:
-        jsonstr =pulljson
-        for pullreq in range(len(jsonstr)):
-            
-            print(pullreq['state'])
+        tmpconsoleprint=constant.str_console_print
+        for pullreq in range(len(pulljson)):
+            if constant.str_display_mode == "console":
+                str_title = pulljson[pullreq]['title']
+                tmpconsoleprint += "\n"+str(pulljson[pullreq]['number'])+"\t\t"+str_title[0:12]
+                tmpconsoleprint += "\t"+pulljson[pullreq]['user']['login']+"\t\t"+pulljson[pullreq]['html_url']+"\t"+pulljson[pullreq]['diff_url']+"\n"
+        tmpconsoleprint+=constant.str_console_footer
+        print(tmpconsoleprint)   
     except Exception:
         print("issue in fetchin the json")
         traceback.print_exc()
